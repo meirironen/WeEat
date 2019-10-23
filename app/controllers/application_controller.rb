417e5ad3e50e8092040eb158a6926be01b0e2507
Ronen Meiri <ronen.meiri@wework.com>
2019-10-23 10:38:51 +0300

    fix issue with blank page loaded on unknown route.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,11 @@
 class ApplicationController < ActionController::API
   include Response
   include ExceptionHandler
+  include ActionController::MimeResponds
 
   def fallback_index_html
-    render :file => 'public/index.html'
+    respond_to do |format|
+      format.html { render body: Rails.root.join('public/index.html').read }
+    end
   end
 end
